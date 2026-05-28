fb2c37849334265f9e71dcc545cd1b8e9538054f
Anurag Singh <Anurag.Singh286600@cognizant.com>
2026-05-28 15:07:55 +0530

    first commit

--- client/README.md
+++
@@ -1,8 +0,0 @@
-// client/src/environments/environment.ts
-
-
-
-export const environment = {
-  baseUrl: 'http://localhost:7000',
-  mapApiKey: 'pk.2291756e6d48580b693a0848389717a5'
-};
--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
-const { getProfile, editProfile } = require("../controllers/userController");
-
-router.get("/profile", authMiddleware, getProfile);
-router.patch("/editProfile", authMiddleware, editProfile);
-
-module.exports = router;
